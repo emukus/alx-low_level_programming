1d88f013b1b575738c0475b756eb307191b452d6
emukus <mukuna.eugene@gmail.com>
2022-04-12 17:24:46 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: contains value to be compared
+ * Return: 1 if c is lower case or 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
+
