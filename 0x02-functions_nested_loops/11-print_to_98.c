72c21fe41d50946c9a116ba32632e6b8ce80525d
emukus <mukuna.eugene@gmail.com>
2022-04-12 19:09:49 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: contains the starting integer value
+ * Return: Always 0 (Successful/correct code)
+ */
+
+void print_to_98(int n);
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+	while ( < 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+printf("98");
+putchar('\n');
+}
