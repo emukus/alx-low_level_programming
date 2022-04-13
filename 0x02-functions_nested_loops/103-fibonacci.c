82a1fca500a4f1ee4030b6b4d4fcdd1636efc4cd
emukus <mukuna.eugene@gmail.com>
2022-04-13 14:24:25 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -16,12 +16,14 @@ int main(void)
 		fibsum = fib1 + fib2;
 		if (fibsum > 400000)
 			break;
+
 		if ((fibsum % 2) == 0)
 			tot_sum += fibsum;
+
 		fib1 = fib2;
 		fib2 = fibsum;
 	}
-	printf("%0.f\n", tot_sum);
+	printf("%.0f\n", tot_sum);
 
 	return (0);
 }
