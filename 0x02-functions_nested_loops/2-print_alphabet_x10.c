5a0277eeaab8e437514ca117165d28aa3fbdbca2
emukus <mukuna.eugene@gmail.com>
2022-04-12 17:18:00 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,11 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char alphabet = 'a';
+	char alphabet;
 
 	while (count < 10)
 	{
+		alphabet = 'a';
 		while (alphabet <= 'z')
 		{
 			_putchar(alphabet);
