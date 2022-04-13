5c4a1794fbaa9b686f4285dce1e360c32e2816a7
emukus <mukuna.eugene@gmail.com>
2022-04-12 18:44:44 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: contains first integer
+ * @b: contains second integer
+ * Return: Always 0 (Successful/correct code)
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
