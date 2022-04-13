c18039add00a8aad9c759a69afb8ed49e39accf5
emukus <mukuna.eugene@gmail.com>
2022-04-12 18:41:14 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, staring with 0
+ *
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int a = 0;
+	int b;
+	int multiple;
+
+	while (a <= 9)
+	{
+		multiple = a * b;
+		if (b == 0)
+		{
+			_putchar('0' + multiple);
+		}
+		else if (multiple < 10)
+		{
+			_putchar(' ');
+			_putchar('0' + multiple);
+		}
+		else
+		{
+			_putchar('0' + multiple / 10);
+			_putchar('0' + multiple % 10);
+		}
+		if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		b++;
+	}
+	_putchar('\n');
+	a++;
+}
