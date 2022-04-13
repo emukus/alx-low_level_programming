e766517ea3f5a882ad5ab3bdf9f04e546caed9c3
emukus <mukuna.eugene@gmail.com>
2022-04-13 13:58:54 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 /**
-* main - main block
-* Description: computes and prints even numbers
+* main - Prints the sum of even-valued Fibonnacci numbers
+* terms not exceeding 4,000,000
+* Return: Always 0 (Successful code)
 */
+
+int main(void)
+{
+	unsinged long fib1 = 0, fib2 = 1, fibsum;
+	float tot_sum;
+
+	while (1)
+	{
+		fibsum = fib1 + fib2;
+		if (fibum > 400000)
+			break;
+		if ((fibsum % 2) == 0)
+			tot_sum += fibsum;
+		fib1 = fib2;
+		fib2 = fibsum:
+	}
+	printf("%0.f\n", tot_sum);
+
+	return (0);
+}
