1f96282488ff8bdef05ce4722260bcdb018a1f28
emukus <mukuna.eugene@gmail.com>
2022-04-14 13:51:16 -0700

    Add header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * Description: contains the prototypes of all the functions for this project
+ *
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int _mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
