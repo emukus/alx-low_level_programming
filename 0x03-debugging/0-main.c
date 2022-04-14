86f918ca3c97eaf9d1dd69bdf35d1f81d2625bf7
emukus <mukuna.eugene@gmail.com>
2022-04-14 05:10:20 -0700

    Task 0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer is positiv or negative
+ * 
+ * Return: Always 0 (Successful/correct code)
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
