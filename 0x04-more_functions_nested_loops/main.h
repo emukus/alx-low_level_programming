bd047e9a0ab014f993caa4762f4cf717fb3f5523
emukus <mukuna.eugene@gmail.com>
2022-04-14 14:45:06 -0700

    Main header

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,12 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int _mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+int _putchar(char c);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
