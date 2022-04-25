fbd5579f68af96ceac478bf19581aca235b0f22f
emukus <mukuna.eugene@gmail.com>
2022-04-24 17:35:25 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	i = 0;
 
-	while (i < n && src[i] != \'0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
@@ -25,7 +25,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (i < n)
 	{
-		dest[i] = \'0';
+		dest[i] = '\0';
 		i++;
 	}
 
