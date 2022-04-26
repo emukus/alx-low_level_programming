2ba1bc542b40adf670f9b8db934884acbadbf29e
emukus <mukuna.eugene@gmail.com>
2022-04-26 16:53:31 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ * @s: string to be searched
+ * @accept: contains the characters to be searched
+ *
+ * Return: 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int r;
+
+	while (*s)
+	{
+		for (r =0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				return (s);
+			}
+		}
+	}
+}
