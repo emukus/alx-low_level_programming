5975911e8a53ed09190cf67002e023c7bb220106
emukus <mukuna.eugene@gmail.com>
2022-04-28 15:01:06 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string, followed by a new line 
+ * _puts_recursion - function that prints a string, followed by a new line
  * @s: string
  *
  * Return: 0
@@ -14,7 +14,7 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	
-	if(*s == '\0')
+
+	if (*s == '\0')
 		_putchar('\n')
 }
