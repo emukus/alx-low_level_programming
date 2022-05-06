f990e424941e37f48c39906fe55600e78efd99b8
emukus <mukuna.eugene@gmail.com>
2022-05-06 16:11:09 -0700

    It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * free_grid - function that frees a 2d grid previous crated by alloc_grid function
+ * free_grid - function that frees a 2d grid previously created
+ * by alloc_grid function
  * @grid: the 2d array to be freed
  * @height: height of the grid
  *
