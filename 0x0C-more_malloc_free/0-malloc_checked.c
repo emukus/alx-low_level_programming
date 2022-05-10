084528a40b25e5a65f9ec181e450757a917121a0
emukus <mukuna.eugene@gmail.com>
2022-05-10 15:54:32 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory 
+ * or 98 (normal process termination status value)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
