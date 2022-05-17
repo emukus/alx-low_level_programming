13f573c62310f08b676822be9e5738b35111906e
emukus <mukuna.eugene@gmail.com>
2022-05-17 13:11:09 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,16 +1,15 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - function that prints anything
- * @format: a list of types of arguments passed to the function
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
  *
- * Return: nothing
+ * Return: no return.
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i = 0, j, k = 0;
+	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
@@ -20,7 +19,7 @@ void print_all(const char * const format, ...)
 		j = 0;
 		while (t_arg[j])
 		{
-			if (format[i] == t_arg[j] && k)
+			if (format[i] == t_arg[j] && c)
 			{
 				printf(", ");
 				break;
@@ -28,24 +27,24 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-			case 'k':
-				printf("%c", va_arg(ap, int)), k = 1;
+		case 'c':
+			printf("%c", va_arg(ap, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(ap, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
 				break;
-			case 'i':
-				printf("%d", va_arg(ap, int)), k = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double)), k = 1;
-				break;
-			case 's':
-				str = va_arg(ap, char *), k = 1;
-				if (!str)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
+			}
+			printf("%s", str);
+			break;
 		} i++;
 	}
 	printf("\n"), va_end(ap);
