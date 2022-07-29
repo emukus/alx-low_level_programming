bd7f903b97048e2244bf4f674138cdbe5013f5bc
emukus <mukuna.eugene@gmail.com>
2022-07-29 18:54:56 +0000

    >>> print(ht)

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -17,17 +17,17 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		printf("{");
 		for (idx = 0; idx < ht->size; idx++)
+		{
+			node = ht->array[idx];
+			while (node != NULL)
 			{
-				node = ht->array[idx];
-				while (node != NULL)
-				{
-					if (i != 0)
-						printf(", ");
-					i = 1;
-					printf("'%s': '%s'", node->key, node->value);
-					node = node->next;
-				}
+				if (i != 0)
+					printf(", ");
+				i = 1;
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
 			}
+		}
 		printf("}\n");
 	}
 }
