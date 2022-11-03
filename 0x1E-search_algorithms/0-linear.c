f6326adb63f054b3ea8b9432a9d54755020477e2
emukus <mukuna.eugene@gmail.com>
2022-11-03 01:38:27 +0000

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,18 +13,18 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i = 0;
+	size_t i = 0;
 
-        if (size == 0 || !array)
-                return (-1);
+	if (size == 0 || !array)
+		return (-1);
 
-        while (i < size)
-        {
-                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                        return (i);
-                i++;
-        }
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
 
-        return (-1);
+	return (-1);
 }
